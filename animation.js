8db907d1507012cdc57763bbc2b1da8b4b01ef44
Lenovo <priyangspatel12345@gmail.com>
2021-02-12 00:01:34 +0530

    upto gallery

--- animation.js
+++ animation.js
@@ -30,4 +30,25 @@ $(document).ready(function(){
             $('.mission-text').removeClass('fromRight');
         }
     })
+
+
+
+
+    $('.gallery-list-item').click(function(){
+        let value=$(this).attr('data-filter');
+        if(value==="all"){
+            $('.filter').show(300);
+        }
+        else{
+            $('.filter').not('.'+value).hide(300)
+            $('.filter').filter('.'+value).show(300)
+        }
+    })
+
+
+    $('.gallery-list-item').click(function(){
+        $(this).addClass('active-item').siblings().removeClass('active-item')
+    })
+
+
 });
