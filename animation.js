60a826229bea21016e8b0443ba01c28c705fdcd7
Lenovo <priyangspatel12345@gmail.com>
2021-02-15 01:37:49 +0530

    Completed

--- animation.js
+++ animation.js
@@ -51,4 +51,29 @@ $(document).ready(function(){
     })
 
 
+
+
+
+    
+    $(window).scroll(function(){
+        let position =$(this).scrollTop();
+        if(position>=4300)
+        {
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        }
+        else{
+
+            $('.card-1').removeClass('moveFromLeft');
+            $('.card-2').removeClass('moveFromBottom');
+            $('.card-3').removeClass('moveFromRight');
+
+           
+        }
+    })
+
+
+
+
 });
